b6e2f41b38ebc6490b006be9d8b45a049b88eeea
Rafael Cristofali <cristofalirafael@gmail.com>
2025-04-29 19:35:23 -0300

    Add enunciados

--- main.py
+++ main.py
@@ -10,7 +10,18 @@ RM553384 - Maria Julia
 RM553266 - Hugo Santos
 '''
 
-''' Exercício 1 '''
+'''
+Exercício 1 
+Escreva uma função recursiva chamada ‘contar_digitos_pares’que receba
+um número inteiro positivo e retorne a quantidade de dígitos pares no número.
+Exemplo de Entrada e Saída:
+  Entrada: 2486
+  Saída: 4 (todos os dígitos são pares: 2, 4, 8, 6)
+  Entrada: 1357
+  Saída: 0 (nenhum dígito é par)
+  Entrada: 1234
+  Saída: 2 (dígitos pares: 2, 4)
+'''
 # def contar_digitos_pares(digit):
 #     if (digit == 0):
 #         return 0
@@ -28,7 +39,10 @@ RM553266 - Hugo Santos
 
 
 
-''' Exercício 2 '''
+'''
+Exercício 2
+Crie uma função chamada find_max na lista ligada. Essa função deve retornar qual o maior elemento armazenado na lista. 
+'''
 # def find_max(listaLigada):
 #     current = listaLigada.head
 #
@@ -48,7 +62,10 @@ RM553266 - Hugo Santos
 # lista.insert_at_end(50)
 # print(find_max(lista))
 
-''' Exercício 3 '''
+'''
+Exercício 3
+Considerando as listas ligadas, crie uma função chamada insert_after, que insere um novo dado após um valor específico. Essa função deve receber um dado a ser inserido e um dado a ser buscado. Quando ela encontrar o dado buscado deverá inserir o dado a ser inserido após esse dado. Se o dado de busca não for encontrado na lista, o novo dado é inserido no final da lista.
+'''
 # def insert_after(listaLigada, dataToFind, insertData):
 #     current = listaLigada.head
 #     while current:
